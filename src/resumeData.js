d3d19ea5f877cb0f716ce4a79977a74f71998cf1
Max <maxkajiwara@gmail.com>
2019-05-14 19:06:50 -0700

    More changes

--- src/resumeData.js
+++ src/resumeData.js
@@ -24,7 +24,7 @@ let resumeData = {
 		}
 	],
 	aboutme:
-		"I'm a recent graduate of Lambda School's Full Stack Web Dev track, having experience working on many group projects in varied roles. I enjoy learning new tools and improving my existing knowledge of languages, libraries, and techniques. I grew up in Hawaii but moved to the Bay Area to participate in the ongoing tech revolution.",
+		"I'm a recent graduate of Lambda School's Full Stack Web Dev track, having experience working on many group projects in a variety of roles. I enjoy learning new tools and improving my existing knowledge of languages, libraries, and techniques. I grew up in Hawaii but moved to the Bay Area to participate in the ongoing tech revolution.",
 	address: 'San Francisco',
 	website: 'maxkajiwar@gmail.com',
 	education: [
@@ -55,7 +55,6 @@ let resumeData = {
 				'Automatically traverses an unknown map of rooms with an arbitrarily connected layout\n● Explores an arbitrary graph using depth first traversal and breadth first search to find unexplored exits, storing all known rooms and connections in state and localstorage\n● Executes actions at an interval determined by the cooldown response from the server\n● Generates a graphic visualization of the map in real time\n● Performs operations with minimal time complexity'
 		}
 	],
-	skillsDescription: 'Your skills here',
 	skills: [
 		{
 			skillname: 'JavaScript'
@@ -78,39 +77,22 @@ let resumeData = {
 	],
 	portfolio: [
 		{
-			name: 'project1',
-			description: 'mobileapp',
-			imgurl: 'images/portfolio/phone.jpg',
+			name: 'Qrafts',
+			description: `A Rate-my-DIY web app`,
+			imgurl: 'images/portfolio/qrafts.png',
 			projectUrl: 'http://www.qrafts.app'
-		},
-		{
-			name: 'project2',
-			description: 'mobileapp',
-			imgurl: 'images/portfolio/project.jpg',
-			projectUrl: 'images/portfolio/phone.jpg'
-		},
-		{
-			name: 'project3',
-			description: 'mobileapp',
-			imgurl: 'images/portfolio/project2.png',
-			projectUrl: 'images/portfolio/phone.jpg'
-		},
-		{
-			name: 'project4',
-			description: 'mobileapp',
-			imgurl: 'images/portfolio/phone.jpg',
-			projectUrl: 'images/portfolio/phone.jpg'
 		}
 	],
 	testimonials: [
 		{
-			description: 'This is a sample testimonial',
-			name: 'Some technical guy'
-		},
-		{
-			description: 'This is a sample testimonial',
-			name: 'Some technical guy'
+			description: `HUGE props to @MaxKajiwara
+			for being the glue holding our team together. As  Team Lead, his experience was absolutely invaluable. He jumped in at every juncture to push with our team hand-in-hand, answering questions and pair programming. You should hire him - FAST!`,
+			name: 'Josh Armantrout, @RVAtrout'
 		}
+		// {
+		// 	description: 'This is a sample testimonial',
+		// 	name: 'Some technical guy'
+		// }
 	]
 };
 

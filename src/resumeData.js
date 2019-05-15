ccc54639d148d1bd3a39dfd7322562928e1a395f
Max <maxkajiwara@gmail.com>
2019-05-14 17:28:23 -0700

    Personalize everything

--- src/resumeData.js
+++ src/resumeData.js
@@ -1,106 +1,117 @@
 let resumeData = {
-    "imagebaseurl":"https://rbhatia46.github.io/",
-    "name": "Rahul Bhatia",
-    "role": "Frontend Developer and Data Scientist",
-    "linkedinId":"Your LinkedIn Id",
-    "skypeid": "Your skypeid",
-    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
-    "socialLinks":[
-        {
-          "name":"linkedin",
-          "url":"https://www.linkedin.com/in/rahul-bhatia-67ba08121/",
-          "className":"fa fa-linkedin"
-        },
-        {
-          "name":"github",
-          "url":"http://github.com/rbhatia46",
-          "className":"fa fa-github"
-        },
-        {
-          "name":"skype",
-          "url":"http://twitter.com/rbhatia46",
-          "className":"fa fa-twitter"
-        }
-      ],
-    "aboutme":"I am currently a pre-final year student at The LNM Institute of Information Technology and pursuing my B.Tech from here. I am a self taught Full Stack Web Developer, currently diving deeper into Machine Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
-    "address":"India",
-    "website":"https://rbhatia46.github.io",
-    "education":[
-      {
-        "UniversityName":"The LNM Insitute of Information Technology",
-        "specialization":"Some specialization",
-        "MonthOfPassing":"Aug",
-        "YearOfPassing":"2020",
-        "Achievements":"Some Achievements"
-      },
-      {
-        "UniversityName":"Some University",
-        "specialization":"Some specialization",
-        "MonthOfPassing":"Jan",
-        "YearOfPassing":"2018",
-        "Achievements":"Some Achievements"
-      }
-    ],
-    "work":[
-      {
-        "CompanyName":"Some Company",
-        "specialization":"Some specialization",
-        "MonthOfLeaving":"Jan",
-        "YearOfLeaving":"2018",
-        "Achievements":"Some Achievements"
-      },
-      {
-        "CompanyName":"Some Company",
-        "specialization":"Some specialization",
-        "MonthOfLeaving":"Jan",
-        "YearOfLeaving":"2018",
-        "Achievements":"Some Achievements"
-      }
-    ],
-    "skillsDescription":"Your skills here",
-    "skills":[
-      {
-        "skillname":"HTML5"
-      },
-      {
-        "skillname":"CSS"
-      },
-      {
-        "skillname":"Reactjs"
-      }
-    ],
-    "portfolio":[
-      {
-        "name":"project1",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/phone.jpg"
-      },
-      {
-        "name":"project2",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/project.jpg"
-      },
-      {
-        "name":"project3",
-        "description":"mobileapp",  
-        "imgurl":"images/portfolio/project2.png"
-      },
-      {
-        "name":"project4",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/phone.jpg"
-      }
-    ],
-    "testimonials":[
-      {
-        "description":"This is a sample testimonial",
-        "name":"Some technical guy"
-      },
-      {
-        "description":"This is a sample testimonial",
-        "name":"Some technical guy"
-      }
-    ]
-  }
-  
-  export default resumeData
+	imagebaseurl: 'https://rbhatia46.github.io/',
+	name: 'Max Kajiwara',
+	role: 'Software Developer with full stack web dev experience',
+	linkedinId: 'maxkajiwara',
+	email: 'maxkajiwara@gmail.com',
+	roleDescription:
+		'I like building intuitive experiences for users on the frontend or developers on the backend. I try to write applications that anticipate your expectations.',
+	socialLinks: [
+		{
+			name: 'linkedin',
+			url: 'https://www.linkedin.com/in/maxkajiwara/',
+			className: 'fa fa-linkedin'
+		},
+		{
+			name: 'github',
+			url: 'http://github.com/maxkajiwara',
+			className: 'fa fa-github'
+		},
+		{
+			name: 'skype',
+			url: 'http://twitter.com/maxkajiwara',
+			className: 'fa fa-twitter'
+		}
+	],
+	aboutme:
+		"I'm a recent graduate of Lambda School's Full Stack Web Dev track, having experience working on many group projects in varied roles. I enjoy learning new tools and improving my existing knowledge of languages, libraries, and techniques. I grew up in Hawaii but moved to the Bay Area to participate in the ongoing tech revolution.",
+	address: 'San Francisco',
+	website: 'maxkajiwar@gmail.com',
+	education: [
+		{
+			UniversityName: 'Lambda School',
+			specialization: 'Full Stack Web',
+			MonthOfPassing: 'March',
+			YearOfPassing: '2019',
+			Achievements:
+				'Lambda School is a 7+ month Computer Science & Software Engineering Academy that provides an immersive (40+ hours/week), hands-on, and project-based curriculum. Students experience Agile software development and Git workflow on all projects from day one.'
+		}
+	],
+	work: [
+		{
+			CompanyName: 'Qrafts',
+			specialization: '',
+			MonthOfLeaving: '',
+			YearOfLeaving: '',
+			Achievements:
+				'A site for users to find, rate, and comment on DIY projects submitted by other users\n● Planned and built intuitive components that behave according to user expectations\n● Created various protected endpoints with sophisticated exception handling\n● Designed and set up a data normalized relational database\n● Wrote complex, atomic transactions to handle simultaneous updates to many tables\n● Integrated third party authentication with Auth0'
+		},
+		{
+			CompanyName: 'Dungeon Traverser',
+			specialization: '',
+			MonthOfLeaving: '',
+			YearOfLeaving: '',
+			Achievements:
+				'Automatically traverses an unknown map of rooms with an arbitrarily connected layout\n● Explores an arbitrary graph using depth first traversal and breadth first search to find unexplored exits, storing all known rooms and connections in state and localstorage\n● Executes actions at an interval determined by the cooldown response from the server\n● Generates a graphic visualization of the map in real time\n● Performs operations with minimal time complexity'
+		}
+	],
+	skillsDescription: 'Your skills here',
+	skills: [
+		{
+			skillname: 'JavaScript'
+		},
+		{
+			skillname: 'Python'
+		},
+		{
+			skillname: 'Reactjs'
+		},
+		{
+			skillname: 'Nodejs'
+		},
+		{
+			skillname: 'HTML5'
+		},
+		{
+			skillname: 'CSS'
+		}
+	],
+	portfolio: [
+		{
+			name: 'project1',
+			description: 'mobileapp',
+			imgurl: 'images/portfolio/phone.jpg',
+			projectUrl: 'http://www.qrafts.app'
+		},
+		{
+			name: 'project2',
+			description: 'mobileapp',
+			imgurl: 'images/portfolio/project.jpg',
+			projectUrl: 'images/portfolio/phone.jpg'
+		},
+		{
+			name: 'project3',
+			description: 'mobileapp',
+			imgurl: 'images/portfolio/project2.png',
+			projectUrl: 'images/portfolio/phone.jpg'
+		},
+		{
+			name: 'project4',
+			description: 'mobileapp',
+			imgurl: 'images/portfolio/phone.jpg',
+			projectUrl: 'images/portfolio/phone.jpg'
+		}
+	],
+	testimonials: [
+		{
+			description: 'This is a sample testimonial',
+			name: 'Some technical guy'
+		},
+		{
+			description: 'This is a sample testimonial',
+			name: 'Some technical guy'
+		}
+	]
+};
+
+export default resumeData;

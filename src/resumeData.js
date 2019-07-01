50a046e00ed05f3d51853698420508fd128edde2
Max <maxkajiwara@gmail.com>
2019-06-30 18:46:14 -0700

    Update portfolio

--- src/resumeData.js
+++ src/resumeData.js
@@ -1,30 +1,29 @@
 let resumeData = {
 	imagebaseurl: 'https://rbhatia46.github.io/',
 	name: 'Max Kajiwara',
-	role: 'Software Developer with full stack web dev experience',
+	role: `I'm a generalist full stack engineer;`,
 	linkedinId: 'maxkajiwara',
 	email: 'maxkajiwara@gmail.com',
-	roleDescription:
-		'I like building intuitive experiences for users on the frontend or developers on the backend. I try to write applications that anticipate your expectations.',
+	roleDescription: `I like to approach software design from a holistic perspective, building each new feature with consideration for every part of the stack. I also try to imagine myself as a consumer at every step of the process, improving the product by upholding a very low tolerance for awkward, confusing, and inconvenient design.`,
 	socialLinks: [
 		{
 			name: 'linkedin',
 			url: 'https://www.linkedin.com/in/maxkajiwara/',
-			className: 'fa fa-linkedin'
+			className: 'fa fa-linkedin',
 		},
 		{
 			name: 'github',
 			url: 'http://github.com/maxkajiwara',
-			className: 'fa fa-github'
+			className: 'fa fa-github',
 		},
 		{
 			name: 'skype',
 			url: 'http://twitter.com/maxkajiwara',
-			className: 'fa fa-twitter'
-		}
+			className: 'fa fa-twitter',
+		},
 	],
 	aboutme:
-		"I'm a recent graduate of Lambda School's Full Stack Web Dev track, having experience working on many group projects in a variety of roles. I enjoy learning new tools and improving my existing knowledge of languages, libraries, and techniques. I grew up in Hawaii but moved to the Bay Area to participate in the ongoing tech revolution.",
+		"As a graduate of Lambda School's Full Stack Web track, I've had a lot of experience working on group projects in a variety of roles. I enjoy learning new tools and improving my existing knowledge of languages, libraries, and coding techniques. I grew up in Hawaii but moved to the Bay Area because it feels like the right place to be.",
 	address: 'San Francisco',
 	website: 'maxkajiwar@gmail.com',
 	education: [
@@ -33,67 +32,72 @@ let resumeData = {
 			specialization: 'Full Stack Web',
 			MonthOfPassing: 'March',
 			YearOfPassing: '2019',
-			Achievements:
-				'Lambda School is a 7+ month Computer Science & Software Engineering Academy that provides an immersive (40+ hours/week), hands-on, and project-based curriculum. Students experience Agile software development and Git workflow on all projects from day one.'
-		}
+			Achievements: `Lambda School is a 7+ month Computer Science & Software Engineering Academy that provides an immersive (40+ hours/week), hands-on, and project-based curriculum. Students experience Agile software development and Git workflow on all projects from day one.\nGraduates continue working together on teams building fully functional web apps until they're hired.`,
+		},
 	],
 	work: [
+		{
+			CompanyName: 'WellBroomed​',
+			specialization: '',
+			MonthOfLeaving: '',
+			YearOfLeaving: '',
+			Achievements:
+				'A short-term rental property turnover assistant\n● Designed our relational database with various frontend operations kept in mind from the start, such as handling cleaner reassignment requests\n● Built routes that handle complex operations such as updating property checklists while preserving older tasks from previous reservations for stat tracking purposes\n● Created advanced SQL queries to support those routes\n● Designed and implemented a user signup system using invite links, setting partnerships for existing accounts or for new accounts during the signup process\n● Implemented auth0 login and custom user token generation for authorization purposes\n● Built property and guest components using Material-UI and Styled-Components\n● Created editable, user-friendly checklists and time-specific reports for reservations',
+		},
 		{
 			CompanyName: 'Qrafts',
 			specialization: '',
 			MonthOfLeaving: '',
 			YearOfLeaving: '',
 			Achievements:
-				'A site for users to find, rate, and comment on DIY projects submitted by other users\n● Planned and built intuitive components that behave according to user expectations\n● Created various protected endpoints with sophisticated exception handling\n● Designed and set up a data normalized relational database\n● Wrote complex, atomic transactions to handle simultaneous updates to many tables\n● Integrated third party authentication with Auth0'
+				'A site for users to find, rate, and comment on DIY projects submitted by other users\n● Planned and built intuitive components that behave according to user expectations, including project cards, forms, and modals\n● Created various protected endpoints with sophisticated exception handling\n● Designed and set up the data normalized relational database, handling values such as project ratings, average user ratings, reviews, and likes\n● Wrote complex, atomic transactions to handle simultaneous updates to many tables\n● Integrated third party authentication with Auth0 and Passport.js',
 		},
-		{
-			CompanyName: 'Dungeon Traverser',
-			specialization: '',
-			MonthOfLeaving: '',
-			YearOfLeaving: '',
-			Achievements:
-				'Automatically traverses an unknown map of rooms with an arbitrarily connected layout\n● Explores an arbitrary graph using depth first traversal and breadth first search to find unexplored exits, storing all known rooms and connections in state and localstorage\n● Executes actions at an interval determined by the cooldown response from the server\n● Generates a graphic visualization of the map in real time\n● Performs operations with minimal time complexity'
-		}
 	],
 	skills: [
 		{
-			skillname: 'JavaScript'
+			skillname: 'JavaScript',
 		},
 		{
-			skillname: 'Python'
+			skillname: 'Python',
 		},
 		{
-			skillname: 'Reactjs'
+			skillname: 'Reactjs',
 		},
 		{
-			skillname: 'Nodejs'
+			skillname: 'Nodejs',
 		},
 		{
-			skillname: 'HTML5'
+			skillname: 'HTML5',
 		},
 		{
-			skillname: 'CSS'
-		}
+			skillname: 'CSS',
+		},
 	],
 	portfolio: [
+		{
+			name: 'WellBroomed​',
+			description: `A short-term rental property turnover assistant`,
+			imgurl: 'images/portfolio/wellbroomed.png',
+			projectUrl: 'http://www.wellbroomed.com',
+		},
 		{
 			name: 'Qrafts',
 			description: `A Rate-my-DIY web app`,
 			imgurl: 'images/portfolio/qrafts.png',
-			projectUrl: 'http://www.qrafts.app'
-		}
+			projectUrl: 'http://www.qrafts.app',
+		},
 	],
 	testimonials: [
 		{
 			description: `HUGE props to @MaxKajiwara
 			for being the glue holding our team together. As  Team Lead, his experience was absolutely invaluable. He jumped in at every juncture to push with our team hand-in-hand, answering questions and pair programming. You should hire him - FAST!`,
-			name: 'Josh Armantrout, @RVAtrout'
-		}
+			name: 'Josh Armantrout, @RVAtrout',
+		},
 		// {
 		// 	description: 'This is a sample testimonial',
 		// 	name: 'Some technical guy'
 		// }
-	]
+	],
 };
 
 export default resumeData;
